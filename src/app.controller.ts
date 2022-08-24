387a62d0a9501f1efa9e23e27e4bfb6f082c4587
andrewoppo <andrew.oppo@gmail.com>
2022-08-24 18:19:09 +0200

    pipes, interceptors and custom decorators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './user.decorator';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,9 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get()
+  async findOne(@User('firstName') firstName: string) {
+    console.log(`Hello ${firstName}`);
   }
 }
